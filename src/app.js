2bcb07fd2b2c764add8839b043dfaa3c9880d7e5
kangbf <46126319+undefined@users.noreply.github.com>
2022-06-25 03:38:44 +0000

    Push from Test_For_Decorators

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
+// import module elements
 import { LightningElement } from 'lwc';
+// declare class to expose the component
 export default class App extends LightningElement {
    name = 'Electra X4';
    description = 'A sweet bike built for comfort.';
@@ -6,4 +8,11 @@ export default class App extends LightningElement {
    material = 'Steel';
    price = '$2,700';
    pictureUrl = 'https://s3-us-west-1.amazonaws.com/sfdc-demo/ebikes/electrax4.jpg';
+   ready = false;
+   // use lifecycle hook
+   connectedCallback() {
+       setTimeout(() => {
+           this.ready = true;
+       }, 3000);
+   }
 }
